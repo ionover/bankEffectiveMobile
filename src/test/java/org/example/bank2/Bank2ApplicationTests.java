b86bed077d88aa03d578140a91bf805d78711ea7
ionov <prostodliastema@mail.ru>
2026-03-08 22:48:29 +0300

    i

---
+++ src/test/java/org/example/bank2/Bank2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bank2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bank2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
