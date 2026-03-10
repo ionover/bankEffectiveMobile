7277f918f5b9829f5d7910e4031db829e7ce34c7
v.ionov <v.ionov@mycrg.ru>
2026-03-10 11:47:47 +0300

    main

--- src/main/java/org/example/bank2/mapper/CardMapper.java
+++ src/main/java/org/example/bank2/mapper/CardMapper.java
@@ -1,9 +1,7 @@
 package org.example.bank2.mapper;
 
 import org.example.bank2.dto.CardRequest;
-import org.example.bank2.dto.UserRequest;
 import org.example.bank2.entity.Card;
-import org.example.bank2.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
