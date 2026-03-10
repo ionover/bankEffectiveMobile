ffff11052cf7b01933e5625852244a540da36844
v.ionov <v.ionov@mycrg.ru>
2026-03-10 12:03:09 +0300

    main

---
+++ asseptense-test/src/main/java/ogr/exapmle/asseptensetest/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package ogr.exapmle.asseptensetest;
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("ru/mycrg/nordcodes")
+public class RunCucumberTest {
+
+}
