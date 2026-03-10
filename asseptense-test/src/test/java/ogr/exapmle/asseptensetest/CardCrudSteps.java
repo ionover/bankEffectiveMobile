31e9721cafc44cc1bd2baa80a1a81815ea699fe2
v.ionov <v.ionov@mycrg.ru>
2026-03-10 17:02:53 +0300

    main

--- asseptense-test/src/test/java/ogr/exapmle/asseptensetest/CardCrudSteps.java
+++ asseptense-test/src/test/java/ogr/exapmle/asseptensetest/CardCrudSteps.java
@@ -1,15 +1,16 @@
 package ogr.exapmle.asseptensetest;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.example.bank2.dto.CardRequest;
 
 import static io.restassured.RestAssured.given;
 import static ogr.exapmle.asseptensetest.BaseSteps.*;
-import static ogr.exapmle.asseptensetest.BaseSteps.BASE_URL;
 
 public class CardCrudSteps {
 
+    @Given("для последнего пользователя существует карта")
     @When("я создаю карту для последнего пользователя")
     public void createCard() {
         CardRequest cardRequest = new CardRequest(RandomStringUtils.randomAlphabetic(10), userId);
@@ -20,6 +21,10 @@ public class CardCrudSteps {
                 .body(gson.toJson(cardRequest))
                 .when()
                 .post(BASE_URL + "/cards");
+    }
 
+    @When("я меняю статус текущей карты на {string}")
+    public void changeCardStatus(String status) {
+     //TODO ты тут
     }
 }
--- asseptense-test/src/test/java/ogr/exapmle/asseptensetest/UserTemplates.java
+++ asseptense-test/src/test/java/ogr/exapmle/asseptensetest/UserTemplates.java
@@ -10,55 +10,64 @@ public class UserTemplates {
             case "обычный пользователь" -> standardUser();
             case "администратор" -> administrator();
             case "не администратор" -> notAdmin();
+            case "смена статуса карты" -> changeCardStatus();
             default -> throw new IllegalArgumentException("Для '" + template + "' нет шаблона");
         };
     }
 
     public static UserRequest defaultUser() {
-        return new UserRequest(
-                "user_default",
-                "12",
-                "Иван",
-                "Иванов",
-                "Иванович",
-                "+79001234567",
-                false
+        return new UserRequest("user_default",
+                               "12",
+                               "Иван",
+                               "Иванов",
+                               "Иванович",
+                               "+79001234567",
+                               false
         );
     }
 
     public static UserRequest administrator() {
-        return new UserRequest(
-                "admin",
-                "12",
-                "Админ",
-                "Администраторов",
-                "Админович",
-                "+79009999999",
-                true
+        return new UserRequest("admin",
+                               "12",
+                               "Админ",
+                               "Администраторов",
+                               "Админович",
+                               "+79009999999",
+                               true
         );
     }
 
     public static UserRequest notAdmin() {
-        return new UserRequest(
-                "noAdmin@ru",
-                "12",
-                "Не админ",
-                "Не администраторов",
-                "Не админович",
-                "+7900000000",
-                false
+        return new UserRequest("noAdmin@ru",
+                               "12",
+                               "Не админ",
+                               "Не администраторов",
+                               "Не админович",
+                               "+7900000000",
+                               false
         );
     }
 
     public static UserRequest standardUser() {
-        return new UserRequest(
-                "standart@ru",
-                "12",
-                "Обычный",
-                "Обычный",
-                "Обычный",
-                "+7900000000",
-                false
+        return new UserRequest("standart@ru",
+                               "12",
+                               "Обычный",
+                               "Обычный",
+                               "Обычный",
+                               "+7900000000",
+                               false
+        );
+    }
+
+    public static UserRequest changeCardStatus() {
+        return new UserRequest("changeCardStatus@ru",
+                               "12",
+                               "changeCardStatus",
+                               "Менять",
+                               "Статус",
+                               "+7900000000",
+                               false
         );
     }
 }
+
