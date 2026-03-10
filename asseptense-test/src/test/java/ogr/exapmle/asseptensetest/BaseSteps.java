aff0a75838023358af9738e47939321cc5f6e22d
v.ionov <v.ionov@mycrg.ru>
2026-03-10 13:56:54 +0300

    main

--- asseptense-test/src/test/java/ogr/exapmle/asseptensetest/BaseSteps.java
+++ asseptense-test/src/test/java/ogr/exapmle/asseptensetest/BaseSteps.java
@@ -1,11 +1,27 @@
 package ogr.exapmle.asseptensetest;
 
 import io.cucumber.java.en.Given;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 
 public class BaseSteps {
 
-    @Given("пользователь авторизован как {string}")
-    public void doLogin(String username) {
 
+    public Response response;
+
+    @Given("пользователь авторизован как {string} с паролем {string}")
+    public void doLogin(String username,  String password) {
+        LoginDto login = new LoginDto(username, password);
+
+        response = given()
+                .body(gson.toJson(login))
+                .when()
+                .post("http://localhost:8080/oauth/login");
+    }
+
+    @Then("сервер отвечает статусом {int}")
+    public void checkStatus(int status) {
+        Assertion.assertEaquals(response.getStatusCode(), status);
     }
 }
--- asseptense-test/src/test/java/ogr/exapmle/asseptensetest/UserTemplates.java
+++ asseptense-test/src/test/java/ogr/exapmle/asseptensetest/UserTemplates.java
@@ -1,8 +1,74 @@
 package ogr.exapmle.asseptensetest;
 
+import org.example.bank2.dto.UserRequest;
+
 public class UserTemplates {
 
-    public static UserRequest createUserRequest() {
-        return new UserRequest();
+    public static UserRequest getUserTemplate(String template) {
+        return switch (template) {
+            case "по-умолчанию" -> defaultUser();
+            case "администратор" -> administrator();
+            default -> throw new IllegalArgumentException("Для '" + template + "' нет шаблона");
+        };
+    }
+
+    public static UserRequest defaultUser() {
+        return new UserRequest(
+                "user_default",
+                "password123",
+                "Иван",
+                "Иванов",
+                "Иванович",
+                "+79001234567",
+                false
+        );
+    }
+
+    public static UserRequest administrator() {
+        return new UserRequest(
+                "admin",
+                "admin123",
+                "Админ",
+                "Администраторов",
+                "Админович",
+                "+79009999999",
+                true
+        );
+    }
+
+    public static UserRequest userWithoutMiddleName() {
+        return new UserRequest(
+                "user_no_middle",
+                "password123",
+                "Петр",
+                "Петров",
+                null,
+                "+79001111111",
+                false
+        );
+    }
+
+    public static UserRequest userWithoutPhone() {
+        UserRequest user = new UserRequest();
+        user.setLogin("user_no_phone");
+        user.setPassword("password123");
+        user.setName("Сергей");
+        user.setSurname("Сергеев");
+        user.setMiddleName("Сергеевич");
+        user.setIsAdmin(false);
+        return user;
+    }
+
+    public static UserRequest customUser(String login, String password, String name,
+                                         String surname, Boolean isAdmin) {
+        return new UserRequest(
+                login,
+                password,
+                name,
+                surname,
+                null,
+                null,
+                isAdmin
+        );
     }
 }
